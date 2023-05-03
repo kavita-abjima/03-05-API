d3b409271e914af67e68e6d4c0b358540cf178c4
Kavita <kavita.jangir@abjima.com>
2023-05-03 19:05:04 +0530

    API

---
+++ CityInfo3.API/Controllers/CitiesController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        private readonly CitiesDataStore _citiesDataStore;
+
+        public CitiesController(ICityInfo3Repository cityInfo3Repository,IMapper
+            mapper)
+        {
+            _cityInfo3Repository = cityInfo3Repository ??
+                throw new ArgumentNullException(nameof(cityInfo3Repository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            //return Ok(_citiesDataStore.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            //find city
+            var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
+
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+    }
+}
